f409151874ee14261374dcf77b12d0b87a2989f8
Felix von Nagel <fvon-nag@student.42.fr>
2023-09-27 11:51:52 +0200

    fixed heredoc segfault

--- syscommands_utils.c
+++ syscommands_utils.c
@@ -6,7 +6,7 @@
 /*   By: fvon-nag <fvon-nag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 10:58:01 by fvon-nag          #+#    #+#             */
-/*   Updated: 2023/05/02 17:46:31 by melkholy         ###   ########.fr       */
+/*   Updated: 2023/09/27 11:48:10 by fvon-nag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
+	if (!s1 || !s2)
+		return (-1);
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		++i;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
